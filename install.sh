7b2a5da0f751906e225f2c0037ce0de255d39dd9
Tom <tom.lau.974@icloud.com>
2026-03-12 11:05:49 +0100

    ajout du npm install

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "npm install"
+cd client && npm install
+cd ../posts && npm install
+cd ../comments && npm install
+cd ../query && npm install
+cd ../moderation && npm install
+cd ../event-bus && npm install
+
 echo "Docker compose build"
 docker compose build
 
